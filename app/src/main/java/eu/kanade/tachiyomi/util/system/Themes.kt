f7c08e995742da2ca37416c1a80a57d0ace2e975
Jays2Kings <jays@outlook.com>
2021-07-03 03:30:39 -0400

    fixing documentation on themes enum vars

--- app/src/main/java/eu/kanade/tachiyomi/util/system/Themes.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/system/Themes.kt
@@ -221,14 +221,14 @@ enum class Themes(@StyleRes val styleRes: Int, val nightMode: Int, @StringRes va
         else -> Color.parseColor("#C2FFFFFF")
     }
 
-    /** Complies with tabBarIconColor */
+    /** Complies with tabBarIconColor or colorAccent */
     @ColorInt
     val lightActiveTab: Int = when (styleRes) {
         R.style.Theme_Tachiyomi_AllBlue -> lightAppBarText
         else -> lightAccent
     }
 
-    /** Complies with tabBarIconColor (probably night) */
+    /** Complies with tabBarIconColor or colorAccent (probably night) */
     @ColorInt
     val darkActiveTab: Int = when (styleRes) {
         R.style.Theme_Tachiyomi_AllBlue -> darkAppBarText
@@ -240,10 +240,15 @@ enum class Themes(@StyleRes val styleRes: Int, val nightMode: Int, @StringRes va
         @ColorInt val secondaryText: Int,
         @ColorInt val colorBackground: Int,
         @ColorInt val colorAccent: Int,
+        /** Complies with colorSecondary */
         @ColorInt val appBar: Int,
+        /** Complies with actionBarTintColor */
         @ColorInt val appBarText: Int,
+        /** Complies with colorPrimaryVariant */
         @ColorInt val bottomBar: Int,
+        /** Complies with tabBarIconInactive */
         @ColorInt val inactiveTab: Int,
+        /** Complies with tabBarIconColor */
         @ColorInt val activeTab: Int,
     )
 }
