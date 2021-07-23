e185422c9f041a666bb623262d9bad9736e73036
Jays2Kings <jays@outlook.com>
2021-07-23 18:51:14 -0400

    Version 1.2.1

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -96,8 +96,8 @@ object AndroidVersions {
     const val compileSdk = 31
     const val minSdk = 23
     const val targetSdk = 30
-    const val versionCode = 78
-    const val versionName = "1.2.0"
+    const val versionCode = 79
+    const val versionName = "1.2.1"
     const val ndk = "22.0.7026061"
 }
 
