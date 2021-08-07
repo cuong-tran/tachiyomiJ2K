b26c029e40af7c834b93741eeb91d9bf59b065c1
Jays2Kings <jays@outlook.com>
2021-08-06 22:38:22 -0400

    Update to AS Arctic Fox (really gradle)

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ subprojects {
 buildscript {
     dependencies {
         classpath(LegacyPluginClassPath.fireBaseCrashlytics)
-        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("com.android.tools.build:gradle:7.0.0")
         classpath(LegacyPluginClassPath.googleServices)
         classpath(LegacyPluginClassPath.kotlinPlugin)
         classpath("com.google.android.gms:oss-licenses-plugin:0.10.4")
