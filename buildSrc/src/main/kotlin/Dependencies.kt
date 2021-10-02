67a4fa4d6bbf904a21cd78ce5d5e5cbda2711bbb
Jays2Kings <jays@outlook.com>
2021-10-02 12:48:45 -0400

    Version 1.3.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -58,8 +58,8 @@ object AndroidVersions {
     const val compileSdk = 31
     const val minSdk = 23
     const val targetSdk = 30
-    const val versionCode = 81
-    const val versionName = "1.3.1"
+    const val versionCode = 82
+    const val versionName = "1.3.2"
     const val ndk = "22.0.7026061"
 }
 
