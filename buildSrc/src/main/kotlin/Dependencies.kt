51c4253ab16bdcf51370cde2ac33bd6533e960fc
Jays2Kings <jays@outlook.com>
2021-10-17 17:16:13 -0400

    Version 1.4.3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -55,8 +55,8 @@ object AndroidVersions {
     const val compileSdk = 31
     const val minSdk = 23
     const val targetSdk = 30
-    const val versionCode = 84
-    const val versionName = "1.4.2"
+    const val versionCode = 85
+    const val versionName = "1.4.3"
     const val ndk = "22.0.7026061"
 }
 
