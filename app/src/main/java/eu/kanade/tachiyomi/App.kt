281d139068803d6e494eacafc7a1f4443a1c17e7
Jays2Kings <jays@outlook.com>
2022-04-07 04:47:38 -0400

    Try to avoid some Webview-related crashes
    Co-Authored-By: arkon <4098258+arkon@users.noreply.github.com>

--- app/src/main/java/eu/kanade/tachiyomi/App.kt
+++ app/src/main/java/eu/kanade/tachiyomi/App.kt
@@ -8,6 +8,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.DefaultLifecycleObserver
@@ -57,6 +58,12 @@ open class App : Application(), DefaultLifecycleObserver {
         // TLS 1.3 support for Android 10 and below
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
             Security.insertProviderAt(Conscrypt.newProvider(), 1)
+        }
+
+        // Avoid potential crashes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val process = getProcessName()
+            if (packageName != process) WebView.setDataDirectorySuffix(process)
         }
 
         Injekt = InjektScope(DefaultRegistrar())
