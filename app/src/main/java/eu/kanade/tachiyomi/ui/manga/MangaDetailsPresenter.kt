6bc1b205293bb6e3afe942855c8712fa8f2e53ce
Jays2Kings <jays@outlook.com>
2022-04-22 04:51:50 -0400

    Fix crash with updateDownload

--- app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaDetailsPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/manga/MangaDetailsPresenter.kt
@@ -55,6 +55,7 @@ import eu.kanade.tachiyomi.util.storage.DiskUtil
 import eu.kanade.tachiyomi.util.system.ImageUtil
 import eu.kanade.tachiyomi.util.system.executeOnIO
 import eu.kanade.tachiyomi.util.system.launchIO
+import eu.kanade.tachiyomi.util.system.launchUI
 import eu.kanade.tachiyomi.util.system.withUIContext
 import eu.kanade.tachiyomi.widget.TriStateCheckBox
 import kotlinx.coroutines.Dispatchers
@@ -177,7 +178,7 @@ class MangaDetailsPresenter(
 
     override fun updateDownload(download: Download) {
         chapters.find { it.id == download.chapter.id }?.download = download
-        presenterScope.launchIO {
+        presenterScope.launchUI {
             controller?.updateChapterDownload(download)
         }
     }
