955277c4ae147b41f1389fdf31e04b7b70b65980
Jays2Kings <jays@outlook.com>
2022-05-16 15:49:33 -0400

    fix searching in library when no items are found

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryController.kt
@@ -1280,7 +1280,7 @@ class LibraryController(
             adapter.removeAllScrollableHeaders()
         }
         adapter.setFilter(query)
-        if (adapter.itemCount == 0) return true
+        if (presenter.allLibraryItems.isEmpty()) return true
         viewScope.launchUI {
             adapter.performFilterAsync()
         }
