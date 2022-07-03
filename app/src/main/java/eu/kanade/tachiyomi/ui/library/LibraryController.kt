a1320c5c26e0d576651004b8b7cc7f579dd28c3d
Saud-97 <39028181+Saud-97@users.noreply.github.com>
2022-07-03 20:22:17 +0300

    Enabled Show all categories during searching (#1312)
    * Fixed library search filtering - #1311
    
    * Fixed library search filtering - #1303 #1311
    
    * Revert "Fixed library search filtering - #1303 #1311"
    
    This reverts commit 37f771502bf79a2dcea14e48279158a19e45e72c.
    
    * Normalized library search filtering
    
    * Revert "Normalized library search filtering"
    
    This reverts commit e4cc9465546054cba4f61ff98eb219720b174389.
    
    * spacing

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryController.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryController.kt
@@ -167,6 +167,7 @@ class LibraryController(
      * Library search query.
      */
     private var query = ""
+    private var oldShowAllCategories = true
 
     /**
      * Currently selected mangas.
@@ -1265,6 +1266,15 @@ class LibraryController(
     }
 
     fun search(query: String?): Boolean {
+        if (!query.isNullOrBlank() && this.query.isBlank() && !presenter.showAllCategories) {
+            oldShowAllCategories = presenter.showAllCategories
+            preferences.showAllCategories().set(true)
+            presenter.getLibrary()
+        } else if (query.isNullOrBlank() && this.query.isNotBlank() && !oldShowAllCategories) {
+            preferences.showAllCategories().set(oldShowAllCategories)
+            presenter.getLibrary()
+        }
+
         if (query != this.query && !query.isNullOrBlank()) {
             binding.libraryGridRecycler.recycler.scrollToPosition(0)
         }
