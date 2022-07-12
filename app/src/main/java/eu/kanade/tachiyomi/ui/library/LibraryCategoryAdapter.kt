116a19136448d0da5046d422c09a14f066787a21
Jays2Kings <jays@outlook.com>
2022-07-12 17:49:17 -0400

    Vibrate device when library fast scroll changes categories

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryCategoryAdapter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryCategoryAdapter.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.ui.library
 
 import android.os.Build
+import android.view.HapticFeedbackConstants
 import android.view.View
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.IFlexible
@@ -33,6 +34,8 @@ class LibraryCategoryAdapter(val controller: LibraryController?) :
     var showNumber = preferences.categoryNumberOfItems().get()
 
     var showOutline = preferences.outlineOnCovers().get()
+
+    private var lastCategory = ""
 
     val hasActiveFilters: Boolean
         get() = controller?.hasActiveFilters == true
@@ -175,7 +178,10 @@ class LibraryCategoryAdapter(val controller: LibraryController?) :
         val db: DatabaseHelper by injectLazy()
         if (position == itemCount - 1) return recyclerView.context.getString(R.string.bottom)
         return when (val item: IFlexible<*>? = getItem(position)) {
-            is LibraryHeaderItem -> item.category.name
+            is LibraryHeaderItem -> {
+                vibrateOnCategoryChange(item.category.name)
+                item.category.name
+            }
             is LibraryItem -> {
                 val text = if (item.manga.isBlank()) return item.header?.category?.name.orEmpty()
                 else when (getSort(position)) {
@@ -259,6 +265,9 @@ class LibraryCategoryAdapter(val controller: LibraryController?) :
                         getFirstLetter(title)
                     }
                 }
+                if (!isSingleCategory) {
+                    vibrateOnCategoryChange(item.header?.category?.name.orEmpty())
+                }
                 when {
                     isSingleCategory -> text
                     recyclerView.resources.isLTR -> text + " - " + item.header?.category?.name.orEmpty()
@@ -266,6 +275,13 @@ class LibraryCategoryAdapter(val controller: LibraryController?) :
                 }
             }
             else -> ""
+        }
+    }
+
+    private fun vibrateOnCategoryChange(categoryName: String) {
+        if (categoryName != lastCategory) {
+            lastCategory = categoryName
+            recyclerView.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
         }
     }
 
