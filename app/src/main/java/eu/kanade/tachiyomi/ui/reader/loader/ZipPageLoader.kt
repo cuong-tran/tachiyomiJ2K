0e70a4c4051b172e3b82fe377351746aa4e184bc
Jays2Kings <jays@outlook.com>
2022-07-20 13:05:29 -0400

    Specify charset for ZIP chapters when using Android N+
    Co-Authored-By: arkon <4098258+arkon@users.noreply.github.com>

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/ZipPageLoader.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/loader/ZipPageLoader.kt
@@ -1,12 +1,13 @@
 package eu.kanade.tachiyomi.ui.reader.loader
 
+import android.os.Build
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.ui.reader.model.ReaderPage
 import eu.kanade.tachiyomi.util.lang.compareToCaseInsensitiveNaturalOrder
 import eu.kanade.tachiyomi.util.system.ImageUtil
 import rx.Observable
 import java.io.File
-import java.util.zip.ZipEntry
+import java.nio.charset.StandardCharsets
 import java.util.zip.ZipFile
 
 /**
@@ -17,7 +18,11 @@ class ZipPageLoader(file: File) : PageLoader() {
     /**
      * The zip file to load pages from.
      */
-    private val zip = ZipFile(file)
+    private val zip = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        ZipFile(file, StandardCharsets.ISO_8859_1)
+    } else {
+        ZipFile(file)
+    }
 
     /**
      * Recycles this loader and the open zip.
@@ -32,17 +37,16 @@ class ZipPageLoader(file: File) : PageLoader() {
      * comparator.
      */
     override fun getPages(): Observable<List<ReaderPage>> {
-        return zip.entries().toList()
+        return zip.entries().asSequence()
             .filter { !it.isDirectory && ImageUtil.isImage(it.name) { zip.getInputStream(it) } }
-            .sortedWith(Comparator<ZipEntry> { f1, f2 -> f1.name.compareToCaseInsensitiveNaturalOrder(f2.name) })
+            .sortedWith { f1, f2 -> f1.name.compareToCaseInsensitiveNaturalOrder(f2.name) }
             .mapIndexed { i, entry ->
-                val streamFn = { zip.getInputStream(entry) }
                 ReaderPage(i).apply {
-                    stream = streamFn
+                    stream = { zip.getInputStream(entry) }
                     status = Page.READY
                 }
             }
-            .let { Observable.just(it) }
+            .let { Observable.just(it.toList()) }
     }
 
     /**
