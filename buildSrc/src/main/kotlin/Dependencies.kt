63961cbd160c09f7e5da30485e96c0b4505ede77
Jays2Kings <jays@outlook.com>
2023-04-15 13:22:52 -0400

    Version 1.6.5

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,8 +4,8 @@ object AndroidVersions {
     const val compileSdk = 33
     const val minSdk = 23
     const val targetSdk = 30
-    const val versionCode = 102
-    const val versionName = "1.6.4"
+    const val versionCode = 103
+    const val versionName = "1.6.5"
     const val ndk = "23.1.7779620"
     const val kotlin = "1.8.10"
 }
