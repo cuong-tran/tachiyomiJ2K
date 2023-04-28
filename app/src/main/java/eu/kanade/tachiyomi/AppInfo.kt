7fcd744c9822efa3c75c161652bd771de295c4d5
arkon <eugcheung94@gmail.com>
2023-04-28 11:36:17 -0400

    extension-lib 1.5: Add AppInfo#getSupportedImageMimeTypes()

--- app/src/main/java/eu/kanade/tachiyomi/AppInfo.kt
+++ app/src/main/java/eu/kanade/tachiyomi/AppInfo.kt
@@ -1,11 +1,32 @@
 package eu.kanade.tachiyomi
 
+import eu.kanade.tachiyomi.util.system.ImageUtil
+
 /**
  * Used by extensions.
- *
- * @since extension-lib 1.3
  */
 object AppInfo {
-    fun getVersionCode() = BuildConfig.VERSION_CODE
-    fun getVersionName() = BuildConfig.VERSION_NAME
+    /**
+     * Version code of the host application. May be useful for sharing as User-Agent information.
+     * Note that this value differs between forks so logic should not rely on it.
+     *
+     * @since extension-lib 1.3
+     */
+    fun getVersionCode(): Int = BuildConfig.VERSION_CODE
+
+    /**
+     * Version name of the host application. May be useful for sharing as User-Agent information.
+     * Note that this value differs between forks so logic should not rely on it.
+     *
+     * @since extension-lib 1.3
+     */
+    fun getVersionName(): String = BuildConfig.VERSION_NAME
+
+    /**
+     * A list of supported image MIME types by the reader.
+     * e.g. ["image/jpeg", "image/png", ...]
+     *
+     * @since extension-lib 1.5
+     */
+    fun getSupportedImageMimeTypes(): List<String> = ImageUtil.ImageType.values().map { it.mime }
 }
