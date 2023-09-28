b5a0316a64b5a0eb10f7df1f8fd4aa55d3ab0866
Jays2Kings <jays@outlook.com>
2023-09-27 17:26:54 -0700

    Update app version to 1.7.1
    No need for a versioncode update

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,7 +5,7 @@ object AndroidVersions {
     const val minSdk = 23
     const val targetSdk = 34
     const val versionCode = 106
-    const val versionName = "1.7.0"
+    const val versionName = "1.7.1"
     const val ndk = "23.1.7779620"
     const val kotlin = "1.9.10"
 }
