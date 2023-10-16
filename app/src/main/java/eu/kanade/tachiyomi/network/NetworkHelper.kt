436b0b673aba1a13dfe8806f88a316d997c0ca7f
Jays2Kings <jays@outlook.com>
2023-10-16 13:35:04 -0700

    update default user agent

--- app/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
+++ app/src/main/java/eu/kanade/tachiyomi/network/NetworkHelper.kt
@@ -73,6 +73,6 @@ class NetworkHelper(val context: Context) {
     }
 
     companion object {
-        const val DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/108.0"
+        const val DEFAULT_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0"
     }
 }
